f60b38b00f9183d6cb5b2923b5415dad9453f9e0
Izaias Henrique <izaias.rique@gmail.com>
2022-11-30 22:47:31 -0300

    chore: add cors and dotenv

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
 const app = require('express')();
+require('dotenv').config();
+
+app.use(require('express').json());
+app.use(require('cors')());
+
+const port = process.env.PORT || 3030;
+
+app.listen(port, () => {
+  console.log(`😎 Server listening on port ${port}`);
+});
